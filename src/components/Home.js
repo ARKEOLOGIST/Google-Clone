3c8a5e488b5d0fd833a84618a6b1f995a58c8d29
Arkadyuti <bandyopadhyayarkadyuti@gmail.com>
2020-09-30 00:56:16 +0530

    Added main google image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import AppsIcon from '@material-ui/icons/Apps';
+import { Avatar } from '@material-ui/core';
+
+import Google from '../assets/google.png';
 
 function Home() {
     return (
@@ -9,10 +13,15 @@ function Home() {
                 <div className="menu">
                     <Link to="/gmail">Gmail</Link>
                     <Link to="/images">Images</Link>
+                    <AppsIcon/>
+                    <Avatar style={{ width: '35px',height: '35px'}}/>
                 </div>
             </div>
             <div className="main">
+            <img src={Google}/>
+            <div className="inputfield">
                 
+            </div>
             </div>
         </div>
     );
